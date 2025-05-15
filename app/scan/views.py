e043517ae619a7c1969f0d4b9a9866c2bf36c555
brad <167724610+brad99d@users.noreply.github.com>
2025-05-15 11:16:54 +1000

    Adds templates for scan app

--- app/scan/views.py
+++ app/scan/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the scan index.")
+def dashboard(request):
+    template = loader.get_template('scan/dashboard.html')
+    return HttpResponse(template.render({}, request))
+
+def profile(request):
+    template = loader.get_template('scan/profile.html')
+    return HttpResponse(template.render({}, request))
