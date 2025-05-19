38d6b7433b08a6fff05d801f0ea7fc179721b5c4
brad <167724610+brad99d@users.noreply.github.com>
2025-05-19 17:36:11 +1000

    Creates management page

--- app/settings/views.py
+++ app/settings/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
 
 # Create your views here.
 @login_required()
 def profile(request):
-    template = loader.get_template('settings/profile.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'settings/profile.html')
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_view(request):
+    users = User.objects.all()
+    return render(request, 'settings/manage.html', {'users': users})
