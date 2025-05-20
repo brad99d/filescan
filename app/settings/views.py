63a929ee5de5ea0519860678c80ac671dabe8b12
brad <167724610+brad99d@users.noreply.github.com>
2025-05-20 16:35:50 +1000

    Adds detail update forms

--- app/settings/views.py
+++ app/settings/views.py
@@ -3,11 +3,44 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from .forms import UsernameUpdateForm
 
 # Create your views here.
 @login_required()
 def profile(request):
-    return render(request, 'settings/profile.html')
+    user = request.user
+    username_form = UsernameUpdateForm(instance=user)
+    password_form = PasswordChangeForm(user=user)
+    if request.method == 'POST':
+        if 'update_username' in request.POST:
+            success = False
+            username_form = UsernameUpdateForm(request.POST, instance=user)
+            if username_form.is_valid():
+                username_form.save()
+                success = True
+            return render(request, 'settings/profile.html', {
+                'username_success': success,
+                'username_form': username_form,
+                'password_form': PasswordChangeForm(user=user),
+            })
+        elif 'update_password' in request.POST:
+            success = False
+            password_form = PasswordChangeForm(user=user, data=request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)
+                success = True
+            return render(request, 'settings/profile.html', {
+                'password_success': success,
+                'username_form': UsernameUpdateForm(instance=user),
+                'password_form': PasswordChangeForm(user=user),
+            })
+    return render(request, 'settings/profile.html', {
+        'username_form': username_form,
+        'password_form': password_form,
+    })
 
 @user_passes_test(lambda u: u.is_staff)
 def admin_view(request):
