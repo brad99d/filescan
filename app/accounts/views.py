c0e16d279bb417431f1bfa28404537a2015325d0
brad <167724610+brad99d@users.noreply.github.com>
2025-05-21 15:11:00 +1000

    Adds migrations

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your views here.
 def home(request):
@@ -19,3 +22,11 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/sign-up.html', {'form': form})
+
+# give admin to the first user
+@receiver(post_save, sender=User)
+def grant_admin_to_first_user(sender, instance, created, **kwargs):
+    if created and User.objects.count() == 1:
+        instance.is_superuser = True
+        instance.is_staff = True
+        instance.save()
